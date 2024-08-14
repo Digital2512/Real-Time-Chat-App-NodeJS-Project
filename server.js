c37327d47dd61edb92358c582ebcfd0f0d62ceba
Digital2512 <valerieannabella123@gmail.com>
2024-08-14 15:58:29 +0700

    Group Chat and Public Chat
    Users are able to create and join groups and also all users are able to chat at public chat

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ let users = [];
 let messages = {};
 let groups = {};
 let groupMessages = {};
+let publicMessages = [];
 
 app.use(express.static('public'));
 
@@ -26,6 +27,12 @@ io.on('connection', (socket) => {
         console.log(`${username} joined the chat`);
     });
 
+    // socket.on('public message', (message) => {
+    //     const publicMessage = {username: socket.username, message: message};
+    //     publicMessages.push(publicMessage);
+    //     io.emit('public message', publicMessage);
+    // });
+
     socket.on('create group', (groupName) => {
         if (!groups[groupName]){
             groups[groupName] = [];
@@ -33,8 +40,9 @@ io.on('connection', (socket) => {
             // io.emit('group created', groupName);
             groups[groupName].push(socket.id);
             socket.emit('group created', groupName);
+            socket.emit('groups list', Object.keys(groups));
         }else{
-            socket.emit('group error', `Group ${groupName} has been created. Please try another groups name`);
+            socket.emit('alert error', `Group ${groupName} has been created. Please try another groups name`);
         }
     });
 
@@ -45,26 +53,75 @@ io.on('connection', (socket) => {
                 console.log(`User ${socket.username} joined group: ${groupName}`);
             }
             socket.emit('joined group', groupName);
+            socket.emit('groups list', Object.keys(groups));
 
             if(groupMessages[groupName] && groupMessages[groupName].length > 0){
                 socket.emit('group message history', groupMessages[groupName])
             }
         }else{
-            socket.emit('group error', `Group ${groupName} not found. Please try another groups name`);
+            socket.emit('alert error', `Group ${groupName} not found. Please try another groups name`);
         }
     });
 
-    socket.on('group message', ({groupName, message}) => {
-        console.log(`Group Name: ${groupName}, Message: ${message}`);
-        if(groups[groupName]){
-            const groupMessage = {username: socket.username, message: message};
-            groupMessages[groupName].push(groupMessage);
-            groups[groupName].forEach(memberID => {
-                io.to(memberID).emit('group message', groupMessage);
-            });
-        }else{
-            socket.on('group error', `Group ${groupName} has been created. Please try another group's name`);
+    // socket.on('group message', ({groupName, message}) => {
+    //     console.log(`Group Name: ${groupName}, Message: ${message}`);
+    //     if(groups[groupName]){
+    //         const groupMessage = {username: socket.username, message: message};
+    //         groupMessages[groupName].push(groupMessage);
+
+    //         groups[groupName].forEach(memberID => {
+    //             io.to(memberID).emit('group message', groupMessage);
+    //         });
+    //     }else{
+    //         socket.on('alert error', `Group ${groupName} has been created. Please try another group's name`);
+    //     }
+    // });
+
+    socket.on('chat message', ({ message, recipient, user}) => {
+        console.log(`Recipient: ${recipient}`);
+    
+        if (!recipient) {
+            socket.emit('alert error', 'Recipient is not defined.');
+            return;
         }
+    
+        if (recipient === 'public') {
+            const publicMessage = { username: socket.username, message: message };
+            publicMessages.push(publicMessage);
+            io.emit('public message print', publicMessage);
+        } else if (recipient.startsWith('group-')) {
+            const groupName = recipient.slice(6);  // Extract the group name
+            console.log(`Group Name: ${groupName}, Message: ${message}`);
+            if (groups[groupName]) {
+                const groupMessage = { username: socket.username, message: message, groupName: groupName };
+                groupMessages[groupName].push(groupMessage);
+    
+                groups[groupName].forEach(memberID => {
+                    io.to(memberID).emit('group message print', groupMessage);
+                });
+            } else {
+                socket.emit('alert error', `Group ${groupName} does not exist.`);
+            }
+        } else if(user === recipient){
+            const recipientKey = [socket.id, recipient].sort().join('-');
+            if (!messages[recipientKey]) {
+                messages[recipientKey] = [];
+            }
+            messages[recipientKey].push({ username: socket.username, message: message });
+            socket.emit('self private message print', { username: socket.username, message: message, user: user});
+        }else{
+            const recipientKey = [socket.id, recipient].sort().join('-');
+            if (!messages[recipientKey]) {
+                messages[recipientKey] = [];
+            }
+            messages[recipientKey].push({ username: socket.username, message: message });
+            socket.emit('private message print', { username: socket.username, message: message, recipient: user});
+            io.to(recipient).emit('private message print', { username: socket.username, message: message, recipient: recipient});
+        }
+    });
+    
+    socket.on('alert error', (alertMessage)=>{
+        socket.emit('alert error', alertMessage);
     });
 
     socket.on('get group messages', (groupName) => {
@@ -72,25 +129,6 @@ io.on('connection', (socket) => {
             socket.emit('group message history', groupMessages[groupName]);
         }else{
             socket.emit('group message history', []);
-        }
-    });
-
-    socket.on('chat message', ({username, message}) => {
-        const displayName = (username === socket.username) ? 'You' : username;
-        io.emit('chat message', {username: displayName, message});
-    });
-
-    socket.on('private message', ({recipient, message, isSelf}) => {
-        const recipientKey = [socket.id, recipient].sort().join('-');
-        if(!messages[recipientKey]){
-            messages[recipientKey] = [];
-        }
-        messages[recipientKey].push({username: socket.username, message: message});
-
-        if(isSelf){
-            socket.emit('private message', {username: 'You', message: message});
-        }else{
-            io.to(recipient).emit('private message', {username:socket.username, message: message, recipient: recipient});
         }
     });
 
@@ -107,6 +145,23 @@ io.on('connection', (socket) => {
         }
     });
 
+    socket.on('get public messages', () => {
+        socket.emit('public message history', publicMessages);
+    });
+
+    // socket.on('private message', ({recipient, message, isSelf}) => {
+    //     const recipientKey = [socket.id, recipient].sort().join('-');
+    //     if(!messages[recipientKey]){
+    //         messages[recipientKey] = [];
+    //     }
+    //     messages[recipientKey].push({username: socket.username, message: message});
+
+    //     if(isSelf){
+    //         socket.emit('private message', {username: 'You', message: message});
+    //     }else{
+    //         io.to(recipient).emit('private message', {username:socket.username, message: message, recipient: recipient});
+    //     }
+    // });
 
     socket.on('disconnect', () => {
         console.log('A user disconnected');
